7c4b705d0d55516f9ba75d8bebd15a214092c6cb
ImeDev <92722739+ImmerDevloper@users.noreply.github.com>
2025-01-12 00:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.title("Ahpy")
+import pandas as pd
+from io import BytesIO, StringIO
+
+file = st.file_uploader("Upload File", type=["png", "jpg"])
+show_file = st.empty()
+if not file:
+            show_file.info("Please upload a file of type: " + ", ".join(["png", "jpg"]))
+            return
+        content = file.getvalue()
+        if isinstance(file, BytesIO):
+            show_file.image(file)
+        file.close()
+
+st.title("LesionNNet: Spinal Lesion Detection")
+st.write("A 2 stage CNN Model that can detect spinal lesions, Please Upload your X-Ray Scan Image below")
+st.write(" "
+st.write("By Visharad Upadhyay")
